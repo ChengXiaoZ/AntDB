a9162c665acaae5eb33bdf4e9ac0075bd0ac4484
dingqs <dingqs@asiainfo.com>
2016-06-22 09:52:34 +0800

    ADB monitor:在unused_oids中添加moniotr_*.h文件;调整monitor_host.h文件,使其代码格式对齐.

--- src/include/catalog/monitor_host.h
+++ src/include/catalog/monitor_host.h
@@ -18,16 +18,16 @@
 
 CATALOG(monitor_host,4921)
 {
-	Oid 			host_oid;				/* host name */
-	int16 			mh_run_state;			/* host run state */
-	timestamptz 	mh_current_time;		/* host cuttent time */
+    Oid             host_oid;               /* host name */
+    int16           mh_run_state;           /* host run state */
+    timestamptz     mh_current_time;        /* host cuttent time */
     int64           mh_seconds_since_boot;  /* host seconds since boot */
-	int16			mh_cpu_core_total;		/* host cpu total cores */
-	int16			mh_cpu_core_available;	/* host cpu available cores */
-	
+    int16           mh_cpu_core_total;      /* host cpu total cores */
+    int16           mh_cpu_core_available;  /* host cpu available cores */
+    
 #ifdef CATALOG_VARLEN
-	text			mh_system;				/* host system */
-	text			mh_platform_type;		/* host plateform type */
+    text            mh_system;              /* host system */
+    text            mh_platform_type;       /* host plateform type */
 #endif /* CATALOG_VARLEN */
 } FormData_monitor_host;
 
@@ -36,25 +36,25 @@ CATALOG(monitor_host,4921)
 #endif
 
 /* ----------------
- *		Form_mgr_host corresponds to a pointer to a tuple with
- *		the format of mgr_host relation.
+ *      Form_mgr_host corresponds to a pointer to a tuple with
+ *      the format of mgr_host relation.
  * ----------------
  */
 typedef FormData_monitor_host *Form_monitor_host;
 
 /* ----------------
- *		compiler constants for monitor_host
+ *      compiler constants for monitor_host
  * ----------------
  */
-#define Natts_monitor_host							8
-#define Anum_monitor_host_host_oid					1
-#define Anum_monitor_host_mh_run_state				2
-#define Anum_monitor_host_mh_current_time			3
+#define Natts_monitor_host                          8
+#define Anum_monitor_host_host_oid                  1
+#define Anum_monitor_host_mh_run_state              2
+#define Anum_monitor_host_mh_current_time           3
 #define Anum_monitor_host_mh_seconds_since_boot     4
-#define Anum_monitor_host_mh_cpu_core_total			5
-#define Anum_monitor_host_mh_cpu_core_available		6
-#define Anum_monitor_host_mh_system					7
-#define Anum_monitor_host_mh_platform_type			8
+#define Anum_monitor_host_mh_cpu_core_total         5
+#define Anum_monitor_host_mh_cpu_core_available     6
+#define Anum_monitor_host_mh_system                 7
+#define Anum_monitor_host_mh_platform_type          8
 
 
 #endif /* MONITOR_HOST_H */
