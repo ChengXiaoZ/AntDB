ab99fb8c9be6968d335a05d7ed95816e76a75e3f
lvcx <lvcx@asiainfo.com>
2016-08-15 20:09:39 +0800

    修改ag sequence系统表插入函数

--- src/backend/catalog/agtm_sequence.c
+++ src/backend/catalog/agtm_sequence.c
@@ -15,14 +15,22 @@ void AddAgtmSequence(const char* database,
 	HeapTuple	htup;
 	Datum		values[Natts_agtm_sequence];
 	bool		nulls[Natts_agtm_sequence];
-
+	NameData    nameDatabase;
+	NameData    nameSchema;
+	NameData    nameSequence;
+	
 	MemSet(values, 0, sizeof(values));
 	MemSet(nulls, false, sizeof(nulls));
+	
+	namestrcpy(&nameDatabase, database);
+	values[Anum_agtm_sequence_database - 1] = NameGetDatum(&nameDatabase);
 
-	values[Anum_agtm_sequence_database - 1] = CStringGetTextDatum(database);
-	values[Anum_agtm_sequence_schema - 1] = CStringGetTextDatum(schema);
-	values[Anum_agtm_sequence_sequence - 1] = CStringGetTextDatum(sequence);
-		
+	namestrcpy(&nameSchema, schema);
+	values[Anum_agtm_sequence_schema - 1] = NameGetDatum(&nameSchema);
+
+	namestrcpy(&nameSequence, sequence);
+	values[Anum_agtm_sequence_sequence - 1] = NameGetDatum(&nameSequence);
+
 	adbSequence = heap_open(AgtmSequenceRelationId, RowExclusiveLock);
 	htup = heap_form_tuple(RelationGetDescr(adbSequence), values, nulls);
 
@@ -30,7 +38,6 @@ void AddAgtmSequence(const char* database,
 
 	/* add index */
 
-	
 	heap_close(adbSequence, RowExclusiveLock);	
 }
 
