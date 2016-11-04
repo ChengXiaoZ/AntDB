3ee0fceac50aede89e2cb3e8ba709b8c1c7de979
zhangliang10 <zhangliang10@asiainfo.com>
2016-11-04 18:23:26 +0800

    删除 oracle_operator.sql 中关于 oracle.date +/- 操作符的定义

--- src/backend/oraschema/oracle_operator.sql
+++ src/backend/oraschema/oracle_operator.sql
@@ -35,24 +35,4 @@ CREATE OPERATOR oracle.- (
   PROCEDURE = oracle.subtract
 );
 
-DROP OPERATOR IF EXISTS oracle.+ (oracle.date, interval);
-CREATE OPERATOR oracle.+ (
-  LEFTARG   = oracle.date,
-  RIGHTARG  = interval,
-  PROCEDURE = oracle.date_pl_interval
-);
-
-DROP OPERATOR IF EXISTS oracle.+ (interval, oracle.date);
-CREATE OPERATOR oracle.+ (
-  LEFTARG   = interval,
-  RIGHTARG  = oracle.date,
-  PROCEDURE = oracle.interval_pl_date
-);
-
-DROP OPERATOR IF EXISTS oracle.- (oracle.date, interval);
-CREATE OPERATOR oracle.- (
-  LEFTARG   = oracle.date,
-  RIGHTARG  = interval,
-  PROCEDURE = oracle.date_mi_interval
-);
 
