5f32389fd9d314bb6e11929c3a921f7a21155ce7
dingqs <dingqs@asiainfo.com>
2017-03-08 17:36:14 +0800

    adbmanager:消除警告信息 由于整型变量没有赋初值：int exit_status = 0;

--- src/bin/agent/agent_utility.c
+++ src/bin/agent/agent_utility.c
@@ -20,7 +20,7 @@ int exec_shell(const char *exec, StringInfo out)
 	volatile pid_t pid = 0;
 	volatile int pipe_stdout[2] = {-1, -1};
 	volatile int pipe_stderr[2] = {-1, -1};
-	int exit_status;
+	int exit_status = 0;
 	AssertArg(exec && out);
 
 	PG_TRY();
